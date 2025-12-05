45f0bb3e0508045f9af9405c44f9ae5bd8369c30
cw183 <cartierwong24@gmail.com>
2025-12-05 17:41:07 -0500

    Fix import path for VotingArtifact

--- lib/voting.ts
+++ lib/voting.ts
@@ -1,5 +1,5 @@
 // lib/voting.ts
-import VotingArtifact from "../contracts/artifacts/contracts/Voting.sol/Voting.json";
+import VotingArtifact from "../contracts/artifacts/contracts/Voting.sol";
 
 // Replace with the address printed by your terminal after deployment
 export const VOTING_CONTRACT_ADDRESS = "0x8a128022Dd472e743f5D3192214AeB7697F1Cdd9";
